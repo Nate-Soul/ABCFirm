dbac16c9783a3970454a54259e18ce421d86fd98
Nate-Soul <nanithsukpong@gmail.com>
2023-11-11 17:03:27 +0100

    form handling fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,157 +6,199 @@ const Home = () => {
   const [openModal, setOpenModal] = useState(false);
   const [disableSubmitButton, setDisableSubmitbutton] = useState(true);
 
-  const [transcriptionLang, setTranscriptionLang] = useState("en-US");
-  const [resourceFile, setResourceFile] = useState(null);
-  const [resourceLink, setResourceLink] = useState("");
-  const [speakerIdentification, setSpeakerIdentification] = useState(false);
-
-  const [transcriptionLangErr, setTranscriptionLangErr] = useState("");
-  const [resourceFileErr, setResourceFileErr] = useState("");
-  const [resourceLinkErr, setResourceLinkErr] = useState("");
-  const [errors, setErrors] = useState([]);
-
-  // const submitBtn = document.querySelector("#submitBtn");
-
-  const handleModal = (e) => {
+  const toggleModal = (e) => {
     e.preventDefault();
     setOpenModal((prev) => !prev);
-  }; 
+  };
 
-  const validateURL = (url) => {
-    const supportedHosts = ['drive.google.com', 'www.dropbox.com', 'www.youtube.com'];
-    const urlPattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?/;
-  
-    const match = url.match(urlPattern);
-      if (!match) {
-        setResourceLinkErr("Please replace with a valid URL");
-        return false; // Invalid URL
-      } else {
-        setResourceLinkErr("");
+  const [formValues, setFormValues] = useState({
+    transcription_language: "en-US",
+    resource_file: null,
+    resource_link: "",
+    enable_speaker_identification: false,
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleTranscriptionLanguage = (transcription_language) => {
+    if (validateTranscriptionLanguage(transcription_language)) {
+      setFormValues({ ...formValues, transcription_language });
+      activateSubmitButton();
+    }
+  };
+
+  const handleUrlChange = (resource_link) => {
+    if (resource_link) {
+      if (validateURL(resource_link)) {
+        setFormValues({ ...formValues, resource_link });
+        // activateSubmitButton();
       }
-  
-      const hostname = new URL(url).hostname;
-      if (supportedHosts.includes(hostname)) {
-        setResourceLinkErr("");
-        if (hostname === 'drive.google.com' && url.includes('/file/d/')) {
-          return 'Google Drive'; // Google Drive link
-        } else if (hostname === 'www.dropbox.com' && url.includes('/s/')) {
-          return 'Dropbox'; // Dropbox link
-        } else if (hostname === 'www.youtube.com') {
-          return 'YouTube'; // YouTube link
-        }
-      }
-  //aakasha063@gmail.com
-      setResourceLinkErr("URL is valid but not from the supported services");
-      return false; // URL is valid but not from the supported services
+    }
   }
 
-  const isTranscriptionLangValid = () => {
-    if (transcriptionLang.trim() === "") {
-      setTranscriptionLangErr("Please Select a transcription language");
+  const handleFileChange = (e) => {
+    if(e.target.files && e.target.files.length > 0){
+      if (validateFile(e.target.files[0])) {
+        setFormValues({ ...formValues, resource_file: e.target.files[0]});
+        activateSubmitButton();
+      }
+    }
+  };
+
+  const handleSpeakerIdentification = () => {
+    setFormValues({ ...formValues, enable_speaker_identification: !formValues.enable_speaker_identification });
+  }
+
+  const validateTranscriptionLanguage = (transcription_language) => {
+    if (!transcription_language || transcription_language.trim() === ""){
+      setErrors({ ...errors, transcription_language: "Please Select a transcription language" });
       return false;
+    }
+    return true;
+  }
+
+  const validateURL = (url) => {
+
+    let isValid = false;
+
+    const supportedHosts = ['drive.google.com', 'www.dropbox.com', 'www.youtube.com'];
+    const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+  
+    if (!url || !urlPattern.test(url)) {
+      setErrors({ ...errors, resource_link: "Please replace with a valid URL" });
+      isValid = false;
     } else {
-      setTranscriptionLangErr("");
-      return true;
-    }
-  };
-
-  const isResourceFileValid = () => {
-    return resourceFile !== null;
-  };
-
-  const isResourceLinkValid = () => {
-    if (resourceLink.trim() === "") {
-      return false;
+      setErrors({ ...errors, resource_link: "" });
+      isValid = true;
     }
 
-    // Use the validateURL function to check the link
-    const linkValidationResult = validateURL(resourceLink);
+    const hostname = new URL(url).hostname;
+    if (
+      !supportedHosts.includes(hostname) && 
+      (
+        hostname !== 'drive.google.com' && url.includes('/file/d/') || 
+        hostname !== 'www.dropbox.com' && url.includes('/s/') ||
+        hostname !== 'www.youtube.com'
+      )
+    ) {
+      setErrors(
+        { ...errors, resource_link: "URL is valid but not from our supported host: Google or dropbox or Youtube" }
+      );
+      isValid = false;
+    } else {
+      setErrors({...errors, resource_link: ""});
+      isValid = true;
+    }
 
-    // If the validation result is one of the supported services, it's valid
-    return linkValidationResult === 'Google Drive' || linkValidationResult === 'Dropbox' || linkValidationResult === 'YouTube';
+    return isValid;
   };
 
+  const validateFile = (resourceFile) => {
 
-  const validateFields = () => {
-    const validTranscriptionLang = isTranscriptionLangValid();
-    const validResourceFile = isResourceFileValid();
-    const validResourceLink = isResourceLinkValid();
-    
-    return (validTranscriptionLang && (validResourceFile || validResourceLink));
+    let isValid = false;
+
+    const maxSize           = Math.floor(1024 * 1024 * 1024);
+    const acceptedFileTypes = [
+      "audio/mpeg",
+      "video/mp4", 
+      "audio/wav", 
+      "audio/x-caf", 
+      "audio/aiff", 
+      "video/x-msvideo", 
+      "application/vnd.rn-realmedia-vbr", 
+      "video/x-flv", 
+      "audio/x-m4a", 
+      "video/quicktime", 
+      "video/x-ms-wmv", 
+      "video/x-ms-wma"
+    ];
+
+    const fileSize = resourceFile.size;
+    const fileType = resourceFile.type;
+
+    if (fileSize > maxSize) {
+      setErrors({ ...errors, resource_file : "File size shouldn't exceed " + maxSize + " bytes" });
+      isValid = false;
+    } else {
+      setErrors({ ...errors, resource_file : "" });
+      isValid = true;
+    }
+
+    if (!acceptedFileTypes.includes(fileType)) {
+      setErrors(
+        { ...errors, resource_file: "Unsupported file extension. Allowed Extensions are listed above"}
+      );
+      isValid = false;
+    } else {
+      setErrors({ ...errors, resource_file : "" });
+      isValid = true;
+    }
+
+    return isValid;
   };
 
   const activateSubmitButton = () => {
-    setDisableSubmitbutton(!validateFields());
+    setDisableSubmitbutton(Object.keys(errors).length > 0);
   };
 
-  const handleFileSelection = (e) => {
-    const inputedFile = e.target.files[0];
-    const maxSize     = Math.floor(1024 * 1024 * 1024);
-    const allowedExtensions = [".mp3", ".mp4", ".wav", ".caf", ".aiff", ".avi", ".rmvb", ".flv", ".m4a", ".mov", ".wmv", ".wma"];
+  const validateFormFields = () => {
 
-    if (inputedFile){
-      const fileSize = inputedFile.size;
-      const fileExtension = '.' + inputedFile.name.split('.').pop().toLowerCase();
+    let isValid = false;
 
-      if(fileSize > maxSize){
-        setResourceFileErr("File size shouldn't exceed " + maxSize + " bytes");
-        inputedFile.value = "";
-        setResourceFile(null);
-        activateSubmitButton();
-        return;
-      } else if (!allowedExtensions.includes(fileExtension)){
-        setResourceFileErr("Unsupported file extension. Allowed Extensions: " + allowedExtensions.join(", "));
-        inputedFile.value = "";
-        setResourceFile(null);
-        activateSubmitButton();
-        return;
-      } else {
-        setResourceFileErr("");
-        setResourceFile(inputedFile);
-        activateSubmitButton();
-      }
-    } else {
-      setResourceFile(null);
-      setResourceFileErr("");
-      activateSubmitButton();
+    const { transcription_language, resource_file, resource_link } = formValues;
+
+    isValid = validateTranscriptionLanguage(transcription_language);
+    
+    if (resource_link) {
+      isValid = validateURL(resource_link);
     }
+
+    if (resource_file) {
+      isValid = validateFile(resource_file);
+    }
+
+    if (resource_file && resource_link){
+      setErrors(
+        { ...errors, form_submit: "You can't upload a resource file and provide an import link simultaneously" }
+      );
+      isValid = false;
+    } else {
+      setErrors({ ...errors, form_submit: "Please upload a resource file or provide an import link" });
+      isValid = false;
+    }
+
+    return isValid;
   };
 
   const handleTranscriptionForm = async (e) => {
     e.preventDefault();
 
-    if (!isResourceFileValid() && !isResourceLinkValid()) {
-      setErrors(["You must upload a file locally or use an import link"]);
-    } else if (isResourceFileValid() && isResourceLinkValid()) {
-      setErrors(["You cannot upload the resource from a local file and use an import link simultaneously"]);
-    } else if (!isTranscriptionLangValid()) {
-      setErrors(["Please select a transcription language"]);
-    } else {
-      setErrors([]);
-
+    // Check if there are any errors
+    if (validateFormFields(formValues)){
+      activateSubmitButton();
+      // if (errors) return;
       try {
-        const data = {
-          transcription_language: transcriptionLang,
-          resource: resourceFile ? resourceFile : resourceLink,
-          enable_speaker_identification: speakerIdentification
-        }
-        await submitData(data);
+        console.log(formValues);
+        // await submitData(formValues);
         resetForm();
       } catch(err){
         console.log(err);
-        setErrors((prev) => ([...prev, "Something went wrong"]));
+        setErrors({ ...errors, form_submit: "something went wrong" });
         resetForm();
       }
+    } else {
+      setErrors({ ...errors, form_submit: "There's an error some fields. please cross-check and try again" });
     }
   };
 
-
   const resetForm = () => {
-    setResourceFile(null);
-    setResourceLink("");
-    setTranscriptionLang("en-US");
-    setSpeakerIdentification(false);
+    setFormValues({
+      transcription_language: "en-US",
+      resource_file: null,
+      resource_link: "",
+      enable_speaker_identification: false,
+    });
+    setErrors({});
     activateSubmitButton();
   };
 
@@ -165,19 +207,20 @@ const Home = () => {
 
       const formData = new FormData();
       formData.append("transcription_language", data.transcription_language);
-      formData.append("resource", data.resource);
+      formData.append("resource_file", data.resource_file);
+      formData.append("resource_link", data.resource_link);
       formData.append("enable_speaker_identification", data.enable_speaker_identification);
 
-      const res = await fetch("http://localhost:8000/api/transcribe/", {
+      const response = await fetch("http://localhost:8000/api/transcribe/", {
         method: "POST",
         body: formData,
       });
 
-      if (res.ok) {
-        const result = await res.json();
+      if (response.ok) {
+        const result = await response.json();
         console.log(result);
       } else {
-        const error = await res.json();
+        const error = await response.json();
         console.log(error);
       }
     } catch (err) {
@@ -244,7 +287,7 @@ const Home = () => {
           </header>
           <button
             className="w-max bg-blue-700 px-3 py-2 rounded-lg text-white"
-            onClick={(e) => handleModal(e)}
+            onClick={toggleModal}
           >
             Transcribe File
           </button>
@@ -306,7 +349,7 @@ const Home = () => {
           <div className="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 bg-white p-5 rounded-lg shadow-sm shadow-white">
             <span
               className="bi-x-lg cursor-pointer absolute top-4 right-4 hover:text-red-500"
-              onClick={handleModal}
+              onClick={toggleModal}
             ></span>
             <h4 className="font-bold text-xl mb-8">Transcribe File</h4>
             <form
@@ -315,30 +358,30 @@ const Home = () => {
             >
               {/* Transcription Language */}
               <div className="flex flex-col gap-3">
-                <label className="font-medium" htmlFor="transcriptionLang">
+                <label className="font-medium" htmlFor="transcriptionLanguage">
                   Transcription Language
                 </label>
                 <select
                   name="transcription_language"
-                  id="transcriptionLang"
-                  onChange={(e) => setTranscriptionLang(e.target.value)}
+                  id="transcriptionLanguage"
+                  onChange={(e) => handleTranscriptionLanguage(e.target.value)}
                   className="p-3 outline-none border border-gray-100 rounded-lg ring-2 ring-transparent hover:ring-blue-100 focus:ring-blue-200"
                 >
                   <option value="en-US">Default English</option>
                   <option disabled>Upgrade your account to explore more options</option>
                 </select>
-                <small className="text-red-300">{transcriptionLangErr}</small>
+                {errors.transcription_language && (<small className="text-red-300">{errors.transcription_language}</small>)}
               </div>
 
               {/* Resource File */}
               <div className="p-5 border border-gray-100 rounded-lg flex flex-col gap-5 justify-center items-center">
-                <span>{resourceFile ? resourceFile.name : "No file selected"}</span>
+                <span>{formValues.resource_file ? formValues.resource_file.name : "No file selected"}</span>
                 <div className="relative block mt-5 mb-4">
                   <input
                     type="file"
-                    onChange={handleFileSelection}
-                    name="file_location"
-                    id="fileLocation"
+                    onChange={handleFileChange}
+                    name="resource_file"
+                    id="resourceFile"
                     accept=".mp3, .mp4, .wav, .caf, .aiff, .avi, .rmvb, .flv, .m4a, .mov, .wmv, .wma"        
                     max={1024 * 1024 * 1024}
                     className="z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer opacity-0"
@@ -347,47 +390,47 @@ const Home = () => {
                     <span className="bi-cloud text-3xl text-blue-700"></span>
                   </div>
                 </div>
-                <label htmlFor="fileLocation" className="font-medium">
+                <label htmlFor="resourceFile" className="font-medium">
                   <span className="text-blue-700 cursor-pointer">Click to Upload</span> or drag
                   and drop
                 </label>
                 <small className="text-center text-sm text-gray-400">
-                  The maximum file size is 1Gb for audio and 10Gb for videos.{" "}
+                  The maximum file size is 1Gb for both audios and videos.{" "}
                   <br />
                   Supported formats are mp3, mp4, wav, caf, aiff, avi, rmvb,
                   flv, m4a, mov, wmv, wma
                 </small>
-                <small className="text-red-500">{resourceFileErr}</small>
+                {errors.resource_file && (<small className="text-red-500">{errors.resource_file}</small>)}
               </div>
 
               {/* Resource Link */}
               <div className="flex flex-col gap-3">
-                <label className="font-medium" htmlFor="fileLink">
+                <label className="font-medium" htmlFor="resourceLink">
                   Import from link
                 </label>
                 <input
                   type="url"
-                  name="file_link"
-                  id="fileLink"
-                  onChange={(e)=>setResourceLink(e.target.value)}
-                  onBlur={activateSubmitButton}
-                  value={resourceLink}
+                  name="resource_link"
+                  id="resourceLink"
+                  onChange={activateSubmitButton}
+                  onBlur={(e) => handleUrlChange(e.target.value)}
                   placeholder="Paste a Dropbox, Google Drive or YouTube URL here"
                   className="p-3 outline-none border border-gray-100 rounded-lg ring-2 ring-transparent hover:ring-blue-100 focus:ring-blue-200"
                 />
-                <small className="text-red-500">{resourceLinkErr}</small>
+                {errors.resource_link && (<small className="text-red-500">{errors.resource_link}</small>)}
               </div>
 
               {/* Speaker Identification */}
               <div className="inline-flex gap-3 items-center">
                 <input
                   type="checkbox"
-                  name="speaker_identification"
-                  id="speakerIdentification"
-                  onChange={() => setSpeakerIdentification(prev => !prev)}
+                  name="enable_speaker_identification"
+                  id="enableSpeakerIdentification"
+                  onChange={handleSpeakerIdentification}
+                  checked={formValues.enable_speaker_identification}
                 />
                 <label 
-                  htmlFor="speakerIdentification"
+                  htmlFor="enableSpeakerIdentification"
                 >
                   {" "}
                   Speaker identification
@@ -405,9 +448,7 @@ const Home = () => {
               </button>
 
               {/* Display Errors */}
-              {errors && errors.map((err, i) => (
-                <div className="block text-red-500" key={i}>{err}</div>
-              ))}
+              {errors.form_submit && ( <small className="text-red-500">{errors.form_submit}</small> )}
             </form>
           </div>
         </div>
